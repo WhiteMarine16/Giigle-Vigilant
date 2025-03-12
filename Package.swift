4064227d2f97639a50ebaee411af57b8ff295202
WhiteMarine16 <200795319+WhiteMarine16@users.noreply.github.com>
2025-03-12 19:44:41 +0100

    A basic swift app that should work

--- Package.swift
+++ Package.swift
@@ -3,7 +3,18 @@ import PackageDescription
 
 let Package = Package(
   name: "ShellShockCosmicEdition",
+  platforms: [
+    .macOS(.v13),
+    .linux
+  ],
   dependencies: [
-    .Package(url: "https://github.com/tlarsen7572/SDL2.git", from "0.0.8")
-  ]
+    .Package(url: "https://github.com/PureSwift/SDL.git", from "2.0.0")
+  ],
+  targets: [
+    .executableTarget(
+      name: "ShellShockCosmicEdition",
+      dependencies: [
+        .product(name: "SDL2", package: "SDL")
+      ]
+    )
 )
