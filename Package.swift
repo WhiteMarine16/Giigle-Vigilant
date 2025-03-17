d5dc01014376df1fe17c37193314c21542eb35fb
WhiteMarine16 <200795319+WhiteMarine16@users.noreply.github.com>
2025-03-17 18:30:18 +0100

    Update Package.swift
    fix typo

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
-//swift-tools-version:6.0
+// swift-tools-version:6.0
 import PackageDescription 
 
-let Package = Package(
-  name: "ShellShockCosmicEdition",
-  platforms: [
-    .macOS(.v13),
-    .linux
-  ],
-  dependencies: [
-    .Package(url: "https://github.com/KevinVitale/SwiftSDL.git", from "v0.2.0-alpha.26")
-  ],
-  targets: [
-    .executableTarget(
-      name: "ShellShockCosmicEdition",
-      dependencies: [
-        "SwiftSDL"
-      ]
-    )
+let package = Package(
+    name: "ShellShockCosmicEdition",
+    platforms: [
+        .macOS(.v13),
+        .linux
+    ],
+    dependencies: [
+        .package(url: "https://github.com/KevinVitale/SwiftSDL.git", from: "0.2.0-alpha.26")
+    ],
+    targets: [
+        .executableTarget(
+            name: "ShellShockCosmicEdition",
+            dependencies: [
+                "SwiftSDL"
+            ]
+        )
+    ]
 )
